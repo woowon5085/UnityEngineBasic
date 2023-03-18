0b13bc311f998cbbd89b455270afd3797f996345
woowon5085 <119842261+woowon5085@users.noreply.github.com>
2023-03-18 18:33:16 +0900

    Collections
    - MyDynamicArray
    - MyDynamicArrayOfT

--- CSharp/Collections/MyDynamicArrayOfT.cs
+++ CSharp/Collections/MyDynamicArrayOfT.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Collections
 {
-    internal class MyDynamicArray
+    internal class MyDynamicArray<T> : IEnumerable<T>
     {
         public int Count
         {
@@ -27,7 +28,7 @@ namespace Collections
 
         }
 
-        public object this[int index]
+        public T? this[int index]
         {
             get
             {
@@ -41,7 +42,7 @@ namespace Collections
         // const 키워드
         // 상수 키워드, const 키워드가 붙은 변수는 초기화만 가능하며, 상수처럼 사용됨.
         private const int DEFAULT_SIZE = 1;
-        private object[] _data = new object[DEFAULT_SIZE];
+        private T[] _data = new T[DEFAULT_SIZE];
         private int _count; // 현재 자료 갯수 (배열의 길이가 아님)
 
 
@@ -53,14 +54,14 @@ namespace Collections
         // 삽입 알고리즘
         // 일반적인 경우에 O(1)
         // Capacity(배열의 크기) 가 모자랄때는 O(N)
-        public void Add(object item)
+        public void Add(T item)
         {
             // 여유공간이 부족하면 더 큰 크기의 배열을 만들고 기존 데이터 복제후 기존 배열은 날림
             if (_count >= _data.Length)
             {
                 // 더 큰 크기 배열 만듦
                 // (현재 데이터 갯수의 10의 승수 + 1 사이즈 만큼 더 큰 배열을 만듦)
-                object[] tmp = new object[_data.Length + (int)Math.Ceiling(Math.Log10(_data.Length)) + DEFAULT_SIZE];
+                T[] tmp = new T[_data.Length + (int)Math.Ceiling(Math.Log10(_data.Length)) + DEFAULT_SIZE];
 
                 // 기존 데이터 복제
                 for (int i = 0; i < _count; i++)
@@ -77,22 +78,22 @@ namespace Collections
 
         // 탐색 알고리즘
         // O(N)
-        public bool Contains(object item)
+        public bool Contains(T item)
         {
             for (int i = 0; i < _count; i++)
             {
-                if (_data[i] == item)
+                if (Comparer<T>.Default.Compare(_data[i], item) == 0)
                     return true;
             }
 
             return false;
         }
 
-        public int FindIndex(object item)
+        public int FindIndex(T item)
         {
             for (int i = 0; i < _count; i++)
             {
-                if (_data[i] == item)
+                if (Comparer<T>.Default.Compare(_data[i], item) == 0)
                     return i;
             }
 
@@ -126,13 +127,67 @@ namespace Collections
                 _data[i] = _data[i + 1];
             }
             _count--;
-            _data[_count] = default(int);
+            _data[_count] = default(T);
             return true;            
         }
 
-        public bool Remove(object item)
+        public bool Remove(T item)
         {
             return RemoveAt(FindIndex(item));
         }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+
+        public struct Enumerator : IEnumerator<T>
+        {
+
+            public T Current => _current;
+
+            object IEnumerator.Current => _current;
+
+            private T? _current;
+            private int _currentIndex;
+            private MyDynamicArray<T> _outer;
+
+            // inner struct/class 에서 outer class의 멤버에 접근하기 위해서는 참조가 필요하기때문에
+            // 생성자에서 넘겨받음
+            public Enumerator(MyDynamicArray<T> outer)
+            {
+                _outer = outer;
+                _current = default(T);
+                _currentIndex = -1;
+            }        
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_currentIndex + 1 > _outer.Count)
+                    return false;
+
+                _currentIndex++;
+                _current = _outer._data[_currentIndex]; // outer의  private 멤버는 inner 에서 접근 가능 
+                return true;
+            }
+
+            public void Reset()
+            {
+                _current = default(T);
+                _currentIndex = -1;
+            }
+        }
+
     }
 }
--- CSharp/Collections/Program.cs
+++ CSharp/Collections/Program.cs
@@ -1,17 +1,102 @@
 ﻿using Collections;
+using System.Collections;
+using System.Collections.Generic;
 // 자료구조
 // 자료구조에서 공통적으로 구현하는 요소들은
 // 탐색, 삽입, 삭제 알고리즘
 // 현재 자료 갯수
+
+#region 동적 배열 
 MyDynamicArray myDynamicArray = new MyDynamicArray();
 myDynamicArray.Add(3);
 myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(1);
-myDynamicArray.Add(3);
+myDynamicArray.Add(2);
+
+Console.WriteLine();
+for (int i = 0; i < myDynamicArray.Count; i++)
+{
+    Console.Write($"{myDynamicArray[i]}.");
+}
+
+myDynamicArray[0] = 5; // 5에 대한 데이터를 가지는 Object가 힙영역에 새로 할당됨.
+myDynamicArray.RemoveAt(1);
+myDynamicArray.Remove(5); // int 타입을 object 파라미터에 인자로 넘겨주면, object 타입으로 암시적 캐스팅이 일어남
+                          // 즉, 정수 5에 대한 데이터를 가지는 object가 힙 영역에 새로할당됨
+                          // 동적배열에 있으면 지워달라는 함수호출이 됨
+                          // 따라서 기존의 5에 대한 객체를 지우는게 아니라 인자를 넘겨줄때 생긴 객체를 지워달라는 요청이됨..
+
+object int5 = 5;
+myDynamicArray.Add(int5);
+myDynamicArray.Remove(5);
+
+myDynamicArray.RemoveAt(myDynamicArray.FindIndex(x => (int)x == 5));
+
+Console.WriteLine();
+for (int i = 0; i < myDynamicArray.Count; i++)
+{
+    Console.Write($"{myDynamicArray[i]}.");
+}
+
+//object
+// System.Object 타입. C#의 모든 타입의 기반타입
+//System.Object 타입. 이 타입에 다른 객체를 참조시킬때 힙영역에 System Object 타입의 객체가 할당됨
+int a = 5;
+object obj; // System.Object 타입 참조 변수 
+obj = a; //a를 System.Object 타입으로 변환한 객체를 힙영역에 할당하고 참조를 변환함... (Boxing)
+obj = new System.Object(); // 사실이부분을 만들어서 데이터를 넣는거임
+obj = myDynamicArray;
+MyDynamicArray da2 = (MyDynamicArray)obj; // Object 타입을 하위 타입으로 변환 (Unboxing) . 일반적인 대입연산보다 약 4배 느림
+
+
+// C# System.Collections의 동적배열
+//-------------------------------------------------------
+
+ArrayList arrayList = new ArrayList();
+arrayList.Add(3);
+arrayList.Add('d');
+arrayList.Add("안녕");
+
+
+#endregion
+
+
+# region Generic 동적배열
+//내가만든 Generic 동적배열
+//----------------------------------------------------------------------------------------------
+
+MyDynamicArray<double> doubleArray = new MyDynamicArray<double>();
+doubleArray.Add(3.0);
+doubleArray.Add(5.0);
+doubleArray.Add(2.0);
+doubleArray.Add(4.0);
+doubleArray.Remove(5.2);
+
+
+Console.WriteLine("Start enumerating Generic MyDynamic array");
+IEnumerator<double> enumerator = doubleArray.GetEnumerator();
+while (enumerator.MoveNext())
+{
+    Console.Write($"{enumerator.Current}.");
+}
+enumerator.Dispose();
+enumerator.Reset();
+
+//foreach
+//foreach (순회할자료형 현재값 변수 in IEnumerable)
+Console.WriteLine();
+Console.WriteLine("Start enumerating Generic MyDynamic array with foreach loop");
+foreach (double item in doubleArray)
+{
+    Console.WriteLine($"{item}.");
+}
+
+// C# System.Collection.Generic의 동적 배열
+//----------------------------------------------------------------------------------------------
+
+List<double> doubleList = new List<double>();
+
+doubleList.Add(2.1);
+doubleList.RemoveAt(0);
+doubleList.FindIndex(x => x == 3.0); // Lamda
+
+#endregion
